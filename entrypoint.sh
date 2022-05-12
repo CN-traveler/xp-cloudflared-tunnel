07cdab321ebd56391c98047b8f34fc5399122be5
CN-traveler <55753029+CN-traveler@users.noreply.github.com>
2022-05-13 07:05:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 echo 1 | xp
 echo -e "5\n$2\n$2" | xp
 cloudflared service install $1
-
-ls /github/home
-
+rm -rf /www/admin/localhost_80/wwwroot
+ln -s /github/workspace /www/admin/localhost_80/wwwroot
 tail -f /var/log/alternatives.log
